83b0b1ce80ac3a77a4bbcaecde87aab8117c9a24
Raoul v. R <vanruesc@outlook.de>
2018-05-31 16:17:03 +0200

    Version 0.8.0
    NPM failed.

--- build/octree-helper.min.js
+++ build/octree-helper.min.js
@@ -1,5 +1,5 @@
 /**
- * octree-helper v0.7.0 build May 31 2018
+ * octree-helper v0.8.0 build May 31 2018
  * https://github.com/vanruesc/octree-helper
  * Copyright 2018 Raoul van Rüschen, Zlib
  */
