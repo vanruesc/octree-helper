063a14b87557050e7269a88aed3c8d2ec64b8a51
Raoul v. R <vanruesc@users.noreply.github.com>
2019-08-09 18:25:58 +0200

    Version 0.22.0

--- build/octree-helper.js
+++ build/octree-helper.js
@@ -1,5 +1,5 @@
 /**
- * octree-helper v0.21.0 build Wed Jun 26 2019
+ * octree-helper v0.22.0 build Fri Aug 09 2019
  * https://github.com/vanruesc/octree-helper
  * Copyright 2019 Raoul van Rüschen, Zlib
  */
