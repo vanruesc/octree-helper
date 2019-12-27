6d6efc432b445d48d210d204f4ac4517033bfff7
Raoul v. R <vanruesc@users.noreply.github.com>
2019-12-27 23:51:55 +0100

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
+import resolve from "@rollup/plugin-node-resolve";
 import babel from "rollup-plugin-babel";
 import minify from "rollup-plugin-babel-minify";
-import resolve from "rollup-plugin-node-resolve";
 
 const pkg = require("./package.json");
 const date = (new Date()).toDateString();
